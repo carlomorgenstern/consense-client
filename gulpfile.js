9cc85841d4fc078ffdb0c4dc9e72840da864db39
Carlo Morgenstern <carlomorgenstern@gmail.com>
2016-03-11 17:03:00 +0100

    11.03. 17:00 working status

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,68 @@
 var gulp = require('gulp');
-var browserify = require('browserify');
-var watchify = require('watchify');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var uglify = require('gulp-uglify');
-var htmlmin = require('gulp-htmlmin');
-var sourcemaps = require('gulp-sourcemaps');
+
+// utility dependencies
+var del = require('del');
 var gutil = require('gulp-util');
-var sass = require('gulp-sass');
-var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
+
+// needed for auto-refresh and browser syncing
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
+// minify html
+var htmlmin = require('gulp-htmlmin');
+
+// browserify node-like requires and bundle js
+var browserify = require('browserify');
+var ngAnnotate = require('browserify-ngannotate');
+var watchify = require('watchify');
+
+// uglifiy / minify javascript
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+
+// sass compiling and bundle & minify css
+var merge = require('merge-stream');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uncss = require('gulp-uncss');
+var cleanCSS = require('gulp-clean-css');
+
+// source folders
 var src = {
 	html: 'src/*.html',
 	js: 'src/js/**/*.js',
 	jsEntry: 'src/js/app.js',
 	scss: 'src/scss/**/*.scss',
-	css: ['node_modules/angular-material/angular-material.min.css']
+	css: 'src/css/**/*.css',
+	img: 'src/img/**'
 };
 
+// destination folders
 var dest = {
 	dev: {
 		html: 'dev/',
 		js: 'dev/js',
-		css: 'dev/css'
+		css: 'dev/css',
+		img: 'dev/img'
 	},
 	prod: {
 		html: 'prod/',
 		js: 'prod/js',
-		css: 'prod/css'
+		css: 'prod/css',
+		img: 'prod/img'
 	},
 	srcMaps: 'maps'
 };
 
+// browserify instances
 var devBrowserify = browserify({
 	entries: src.jsEntry,
 	cache: {},
 	packageCache: {},
 	plugin: [watchify],
+	transform: [ngAnnotate],
 	debug: true
 });
 
@@ -46,7 +70,16 @@ var prodBrowserify = browserify({
 	entries: src.jsEntry,
 	cache: {},
 	packageCache: {},
-	plugin: [watchify]
+	plugin: [watchify],
+	transform: [ngAnnotate]
+});
+
+gulp.task('clean.dev', function() {
+	return del.sync('dev/');
+});
+
+gulp.task('clean.prod', function() {
+	return del.sync('prod/');
 });
 
 gulp.task('html.dev', function() {
@@ -84,17 +117,20 @@ gulp.task('js.prod', function() {
 		.pipe(source('app.js'))
 		.pipe(buffer())
 		.pipe(uglify())
-		.on('error', gutil.log)
+			.on('error', gutil.log)
 		.pipe(gulp.dest(dest.prod.js))
 		.pipe(reload({
 			stream: true
 		}));
 });
 
-gulp.task('scss.dev', function() {
-	return gulp.src(src.scss)
-		.pipe(sourcemaps.init())
-		.pipe(sass().on('error', sass.logError))
+gulp.task('css.dev', function() {
+	return merge(gulp.src(src.scss)
+			.pipe(sourcemaps.init())
+			.pipe(sass().on('error', sass.logError))
+		, gulp.src(src.css)
+			.pipe(sourcemaps.init()))
+		.pipe(concat('styles.css'))
 		.pipe(sourcemaps.write(dest.srcMaps))
 		.pipe(gulp.dest(dest.dev.css))
 		.pipe(reload({
@@ -102,48 +138,53 @@ gulp.task('scss.dev', function() {
 		}));
 });
 
-gulp.task('scss.prod', function() {
-	return gulp.src(src.scss)
-		.pipe(sass().on('error', sass.logError))
-		.pipe(cleanCSS({
-			compatibility: 'ie9'
-		}))
+gulp.task('css.prod', function() {
+	return merge(gulp.src(src.scss)
+			.pipe(sass().on('error', sass.logError))
+		, gulp.src(src.css))
+		.pipe(concat('styles.css'))
+		/*.pipe(uncss({
+            html: ['dev/index.html'],
+            ignore: [''],
+            timeout: 1000
+        }))*/
+		.pipe(cleanCSS())
 		.pipe(gulp.dest(dest.prod.css))
 		.pipe(reload({
 			stream: true
 		}));
 });
 
-gulp.task('copyCss.dev', function() {
-	return gulp.src(src.css)
-		.pipe(gulp.dest(dest.dev.css));
+gulp.task('copyRes.dev', function() {
+	return gulp.src(src.img)
+		.pipe(gulp.dest(dest.dev.img));
 })
 
-gulp.task('copyCss.prod', function() {
-	return gulp.src(src.css)
-		.pipe(gulp.dest(dest.prod.css));
+gulp.task('copyRes.prod', function() {
+	return gulp.src(src.img)
+		.pipe(gulp.dest(dest.prod.img));
 })
 
-gulp.task('serve.dev', ['html.dev', 'js.dev', 'scss.dev', 'copyCss.dev'], function() {
+gulp.task('serve.dev', ['clean.dev','html.dev', 'js.dev', 'css.dev', 'copyRes.dev'], function() {
 	browserSync({
 		server: './dev'
 	});
 
 	gulp.watch(src.html, ['html.dev', reload]);
 	gulp.watch(src.js, ['js.dev']);
-	gulp.watch(src.scss, ['scss.dev']);
-	gulp.watch(src.css, ['copyCss.dev', reload]);
+	gulp.watch(src.scss, ['css.dev']);
+	gulp.watch(src.img, ['copyRes.dev', reload]);
 });
 
-gulp.task('serve.prod', ['html.prod', 'js.prod', 'scss.prod', 'copyCss.prod'], function() {
+gulp.task('serve.prod', ['clean.prod','html.prod', 'js.prod', 'css.prod', 'copyRes.prod'], function() {
 	browserSync({
 		server: './prod'
 	});
 
 	gulp.watch(src.html, ['html.prod', reload]);
 	gulp.watch(src.js, ['js.prod']);
-	gulp.watch(src.scss, ['scss.prod']);
-	gulp.watch(src.css, ['copyCss.prod', reload]);
+	gulp.watch(src.scss, ['css.prod']);
+	gulp.watch(src.img, ['copyRes.prod', reload]);
 });
 
 gulp.task('default', ['serve.dev']);
--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,96 @@
 var angular = require('angular');
 (function(angular) {
 	angular.module('app', [require('angular-material')])
-		.directive('myDiv', function() {
-			return {
-				template: "TESTEST123"
+		.config(['$mdDateLocaleProvider', function($mdDateLocaleProvider) {
+			// Example of a French localization.
+			$mdDateLocaleProvider.months = ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'];
+			$mdDateLocaleProvider.shortMonths = ['Jan', 'Feb', 'Mär', 'Apr', 'Mai', 'Jun', 'Jul', 'Aug', 'Sep', 'Okt', 'Nov', 'Dez'];
+			$mdDateLocaleProvider.days = ['Sonntag', 'Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag', 'Samstag'];
+			$mdDateLocaleProvider.shortDays = ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'];
+			$mdDateLocaleProvider.firstDayOfWeek = 1;
+			// Example uses moment.js to parse and format dates.
+			//			$mdDateLocaleProvider.parseDate = function(dateString) {
+			//				var m = moment(dateString, 'L', true);
+			//				return m.isValid() ? m.toDate() : new Date(NaN);
+			//			};
+			//			$mdDateLocaleProvider.formatDate = function(date) {
+			//				return moment(date).format('L');
+			//			};
+			$mdDateLocaleProvider.monthHeaderFormatter = function(date) {
+				return $mdDateLocaleProvider.shortMonths[date.getMonth()] + ' ' + date.getFullYear();
 			};
-		});
+
+			$mdDateLocaleProvider.weekNumberFormatter = function(weekNumber) {
+				return 'Woche ' + weekNumber;
+			};
+			$mdDateLocaleProvider.msgCalendar = 'Kalender';
+			$mdDateLocaleProvider.msgOpenCalendar = 'Kalender öffnen';
+		}])
+
+	.controller('AppCtrl',['$scope', '$timeout', function($scope, $timeout) {
+		$scope.myDate = new Date();
+
+
+		// In this example, we set up our model using a class.
+		// Using a plain object works too. All that matters
+		// is that we implement getItemAtIndex and getLength.
+		var DynamicItems = function() {
+			/**
+			 * @type {!Object<?Array>} Data pages, keyed by page number (0-index).
+			 */
+			this.loadedPages = {};
+
+			/** @type {number} Total number of items. */
+			this.numItems = 0;
+
+			/** @const {number} Number of items to fetch per request. */
+			this.PAGE_SIZE = 50;
+
+			this.fetchNumItems_();
+		};
+
+		// Required.
+		DynamicItems.prototype.getItemAtIndex = function(index) {
+			var pageNumber = Math.floor(index / this.PAGE_SIZE);
+			var page = this.loadedPages[pageNumber];
+
+			if (page) {
+				return page[index % this.PAGE_SIZE];
+			} else if (page !== null) {
+				this.fetchPage_(pageNumber);
+			}
+		};
+
+		// Required.
+		DynamicItems.prototype.getLength = function() {
+			return this.numItems;
+		};
+
+		DynamicItems.prototype.fetchPage_ = function(pageNumber) {
+			// Set the page to null so we know it is already being fetched.
+			this.loadedPages[pageNumber] = null;
+
+			// For demo purposes, we simulate loading more items with a timed
+			// promise. In real code, this function would likely contain an
+			// $http request.
+			$timeout(angular.noop, 300).then(angular.bind(this, function() {
+				this.loadedPages[pageNumber] = [];
+				var pageOffset = pageNumber * this.PAGE_SIZE;
+				for (var i = pageOffset; i < pageOffset + this.PAGE_SIZE; i++) {
+					this.loadedPages[pageNumber].push(i);
+				}
+			}));
+		};
+
+		DynamicItems.prototype.fetchNumItems_ = function() {
+			// For demo purposes, we simulate loading the item count with a timed
+			// promise. In real code, this function would likely contain an
+			// $http request.
+			$timeout(angular.noop, 300).then(angular.bind(this, function() {
+				this.numItems = 50000;
+			}));
+		};
+
+		this.dynamicItems = new DynamicItems();
+	}]);
 })(angular);
