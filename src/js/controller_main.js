0f57b8f9493d176a01513d52aed3ddc20fd9abd5
Carlo Morgenstern <carlomorgenstern@gmail.com>
2016-05-29 02:35:10 +0200

    UI Design almost finished

--- src/js/controller_main.js
+++ src/js/controller_main.js
@@ -1,11 +1,13 @@
 'use strict';
-
-module.exports = ['$http', '$q', function($http, $q) {
+var angular = require('angular');
+var moment = require('moment');
+require('../../vendor/js/fullcalendar/de.js');
+module.exports = ['$http', '$timeout', '$q', '$window', 'uiCalendarConfig', function($http, $timeout, $q, $window, uiCalendarConfig) {
 	var ctrl = this;
-
 	// variables
 	ctrl.selectedTab = 0;
 	ctrl.selectedDate = new Date();
+	ctrl.calendarTitle = "";
 	ctrl.eventSources = [];
 
 	// handlers for courses, speakers, rooms
@@ -13,9 +15,75 @@ module.exports = ['$http', '$q', function($http, $q) {
 	ctrl.speakers = createItemHandler('speaker', 'http://localhost:8080/api/speakers');
 	ctrl.rooms = createItemHandler('room', 'http://localhost:8080/api/rooms', "http://localhost:8080/api/room/");
 
+	// calendar configuration
 	ctrl.calendarConfig = {
-		lang: 'de'
+		lang: 'de',
+		header: false,
+		defaultView: 'agendaWeek',
+		views: {
+			agenda: {
+				slotLabelFormat: 'H(:mm) [Uhr]',
+				scrollTime: '08:00:00',
+				minTime: '06:00:00',
+				maxTime: '22:00:00'
+			},
+			agendaWeek: {
+				columnFormat: 'dd DD.MM.'
+			},
+			agendaDay: {
+				columnFormat: 'DD.MM.YYYY'
+			}
+		}
 	};
+
+	// resizing the calendar on window resize
+	$window.addEventListener('resize', function() {
+		var calendarContainer = angular.element(document.querySelector('#calendarContainer'));
+		ctrl.calendarConfig.aspectRatio = calendarContainer.prop('offsetWidth') / calendarContainer.prop('offsetHeight');
+	});
+
+	// methods for controlling the calendar
+	ctrl.changeCalendarView = function(view) {
+		uiCalendarConfig.calendars.eventCalendar.fullCalendar('changeView', view);
+		changeCalendarTitle();
+	};
+
+	ctrl.previousCalendarView = function() {
+		uiCalendarConfig.calendars.eventCalendar.fullCalendar('prev');
+		changeCalendarTitle();
+	};
+
+	ctrl.nextCalendarView = function() {
+		uiCalendarConfig.calendars.eventCalendar.fullCalendar('next');
+		changeCalendarTitle();
+	};
+
+	ctrl.setToday = function() {
+		ctrl.selectedDate = new Date();
+		ctrl.selectedDateChange(ctrl.selectedDate);
+	};
+
+	ctrl.selectedDateChange = function(newDate) {
+		uiCalendarConfig.calendars.eventCalendar.fullCalendar('gotoDate', newDate);
+		changeCalendarTitle();
+	};
+
+	function changeCalendarTitle() {
+		var viewedDate = uiCalendarConfig.calendars.eventCalendar.fullCalendar('getDate');
+		if (uiCalendarConfig.calendars.eventCalendar.fullCalendar('getView').name === 'agendaDay') {
+			ctrl.calendarTitle = moment(viewedDate).format('dddd, [der] D. MMMM YYYY');
+		} else if (uiCalendarConfig.calendars.eventCalendar.fullCalendar('getView').name === 'agendaWeek') {
+			ctrl.calendarTitle = moment(viewedDate).format('[KW] W: ') + moment(viewedDate).startOf('isoWeek').format('D. MMMM') + ' - ' +
+				moment(viewedDate).endOf('isoWeek').format('D. MMMM');
+		}
+	}
+
+	// setup calendar size and title on app startup
+	$timeout(function() {
+		var calendarContainer = angular.element(document.querySelector('#calendarContainer'));
+		ctrl.calendarConfig.aspectRatio = calendarContainer.prop('offsetWidth') / calendarContainer.prop('offsetHeight');
+		ctrl.selectedDateChange(ctrl.selectedDate);
+	});
 
 	function createItemHandler(type, apiCall, apiEventCall) {
 		var allItems = [];
